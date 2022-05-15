1044ff02a3c11b237fa4dae579bf68b4aa745077
Desai Hardik <87645745+imhardikdesai@users.noreply.github.com>
2022-05-15 15:37:15 +0530

    Add Files

---
+++ VoiceMail/app/src/main/java/com/example/myapplication/Config.java
@@ -0,0 +1,6 @@
+package com.example.myapplication;
+
+public class Config {
+    public static String EMAIL ="testmail12158@gmail.com";  //gmail address
+    public static String PASSWORD ="testmail123"; //password
+}
